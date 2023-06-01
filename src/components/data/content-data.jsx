5553057b4a55c8520b8308a40ed002ee8592e502
Smolllll1 <Smolllll192@gmail.com>
2023-06-01 09:37:13 +0300

    first commit1

--- src/components/data/content-data.jsx
+++ src/components/data/content-data.jsx
@@ -9,7 +9,7 @@ const ContentDataProvider = ({ children }) => {
 
     // click cards (people-list-info page, movies-list-info page)
     const [isCardsPeopleId, setIsCardsPeopleId] = useState(parseInt(location?.pathname.split("/")[2]));
-    const [isCardsMoviesId, setIsCardsMoviesId] = useState(parseInt(location?.pathname.split("/")[3]));
+    const [isCardsMoviesId, setIsCardsMoviesId] = useState(parseInt(location?.pathname.split("/")[2]));
 
     // pagination (people page, movies page)
     const [isPagePeople, setIsPagePeople] = useState(location.search ? parseInt(location?.search.split("=")[1]) : 1);
