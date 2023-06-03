6a0edcbb8c10d576adf64e7f78df974126c4bfdc
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-06-03 12:49:39 +0300

    newauth

--- src/components/list-selected-movies/index.jsx
+++ src/components/list-selected-movies/index.jsx
@@ -1,11 +1,14 @@
 import React, { Fragment, useContext } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 import Button from '@mui/material/Button';
 import store from "../../redux/store";
 import { ContentData } from '../data/content-data';
 import { AuthenticationData } from '../data/authentication-data';
 import { axiosBaseUrl } from "../../api/axios";
+
+// GET URL BUY MOVIE
+const BUY_MOVIE_URL = `users/account/`;
 
 // button style buy movie
 const useStyleBtnBuyMovies = {
@@ -24,11 +27,6 @@ const useStyleBtnBuyMovies = {
 }
 
 export const ListSelectedMovies = () => {
-
-    const location = useLocation();
-    const endUrlAccountUsers = location.pathname.split("/")[3];
-    // GET URL BUY MOVIE
-    const BUY_MOVIE_URL = `users/account/${endUrlAccountUsers}/`;
 
     // object store data movies 
     const storeDataMovies = store.getState();
