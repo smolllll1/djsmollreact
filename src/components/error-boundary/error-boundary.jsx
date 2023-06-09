0689a0eea812bf91bc04579f466b2ae01f4c0d83
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-06-09 15:47:12 +0300

    errorboundary

--- src/components/error-boundary/error-boundary.jsx
+++ src/components/error-boundary/error-boundary.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Alert from 'react-bootstrap/Alert';
+import { Link } from "react-router-dom";
 
 class ErrorBoundary extends React.Component {
 
@@ -14,7 +15,7 @@ class ErrorBoundary extends React.Component {
     render() {
         if (this.state.hasError) {
             return <Alert variant="danger">
-                Something went wrong!
+                Something went wrong! <span>go back<Link to={"/"} reloadDocument className="text-decoration-none"> home</Link></span>
             </Alert>
             // return <p>Something went wrong</p>
             // return this.props.fallback;
